29235f17957702848ace2a12a96d8b5af8dea37c
Jean-Philippe Brucker <jean-philippe@linaro.org>
2023-03-01 15:37:35 +0000

    hw/virtio/virtio-dmatest: Add vhost support
    Use the kernel vhost support when requested and supported
    
    Signed-off-by: Jean-Philippe Brucker <jean-philippe@linaro.org>
    Signed-off-by: Pierrick Bouvier <pierrick.bouvier@linaro.org>

--- include/hw/virtio/virtio-dmatest.h
+++ include/hw/virtio/virtio-dmatest.h
@@ -11,17 +11,30 @@
 
 #include "qom/object.h"
 #include "hw/virtio/virtio.h"
+#include "hw/virtio/vhost.h"
 
 #define TYPE_VIRTIO_DMATEST "virtio-dma-test"
 #define TYPE_VIRTIO_DMATEST_PCI "virtio-dma-test-pci"
 
 OBJECT_DECLARE_SIMPLE_TYPE(VirtIODMATest, VIRTIO_DMATEST)
 
+typedef struct VhostDMATestQueue {
+    struct vhost_dev dev;
+    int fd;
+} VhostDMATestQueue;
+
+typedef struct VhostDMATest {
+    struct VhostDMATestQueue *queues;
+    bool started;
+} VhostDMATest;
+
 struct VirtIODMATest {
     VirtIODevice parent_obj;
     VirtQueue **job_vqs;
     uint64_t features;
     uint16_t num_queues;
+    bool use_vhost;
+    VhostDMATest *vhost;
 };
 
 #endif
