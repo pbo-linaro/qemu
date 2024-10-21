295fa39da975922f557536be6a26e4e5a6d798bb
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2024-10-21 09:03:05 -0700

    custom scripts: build/git/misc
    Signed-off-by: Pierrick Bouvier <pierrick.bouvier@linaro.org>

---
+++ publish.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+set -euo pipefail
+
+if [ $# -lt 1 ]; then
+    echo "usage: branch [git-publish options]..."
+    exit 1
+fi
+
+current_branch=$(git branch --show-current)
+patches=$(mktemp -d)
+trap "rm -rf $patches; git checkout $current_branch" EXIT
+
+branch=$1; shift
+base_revision=${branch}_base
+
+git format-patch ${base_revision}..${branch} -o $patches
+git fetch -a upstream
+git checkout -b $branch-publish || git checkout $branch-publish
+git reset --hard upstream/master
+if ! git am $patches/*; then
+    git am --show-current-patch=diff
+    git am --abort
+    exit 1
+fi
+./scripts/checkpatch.pl $(git merge-base upstream/master HEAD)..HEAD
+if ! git publish --base upstream/master "$@"; then
+    exit 1
+fi
+exit 0
