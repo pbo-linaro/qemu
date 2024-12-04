cc15f088698c912bf8d967be94aa2bd99a5f7a60
Michael Prantl <michael.prantl@hotmail.de>
2024-12-04 20:42:50 +0000

    Adapt pcie testdev integration tests for tdisp
    Signed-off-by: Pierrick Bouvier <pierrick.bouvier@linaro.org>

--- tests/qtest/tdisp-test.c
+++ tests/qtest/tdisp-test.c
@@ -5,40 +5,40 @@
 #include "libqos/qgraph.h"
 #include "libqos/pci.h"
 
-typedef struct QPCIeTestDev {
+typedef struct QTDISPTestDev {
     QOSGraphObject obj;
     QPCIDevice dev;
-} QPCIeTestDev;
+} QTDISPTestDev;
 
-static void *pcie_testdev_get_driver(void *obj, const char *interface)
+static void *tdisp_testdev_get_driver(void *obj, const char *interface)
 {
-    QPCIeTestDev *pcie_testdev = obj;
+    QTDISPTestDev *tdisp_testdev = obj;
 
     if (!g_strcmp0(interface, "pci-device")) {
-        return &pcie_testdev->dev;
+        return &tdisp_testdev->dev;
     }
 
-    fprintf(stderr, "%s not present in edu\n", interface);
+    fprintf(stderr, "%s not present in tdisp-testdev\n", interface);
     g_assert_not_reached();
 }
 
-static void *pcie_testdev_create(void *pci_bus, QGuestAllocator *alloc,
+static void *tdisp_testdev_create(void *pci_bus, QGuestAllocator *alloc,
                                  void *addr)
 {
-    QPCIeTestDev *pcie_testdev = g_new0(QPCIeTestDev, 1);
+    QTDISPTestDev *tdisp_testdev = g_new0(QTDISPTestDev, 1);
     QPCIBus *bus = pci_bus;
 
-    qpci_device_init(&pcie_testdev->dev, bus, addr);
-    pcie_testdev->obj.get_driver = pcie_testdev_get_driver;
+    qpci_device_init(&tdisp_testdev->dev, bus, addr);
+    tdisp_testdev->obj.get_driver = tdisp_testdev_get_driver;
 
-    return &pcie_testdev->obj;
+    return &tdisp_testdev->obj;
 }
 
 /* Tests only initialization so far. TODO: Replace with functional tests */
 static void nop(void *obj, void *data, QGuestAllocator *alloc)
 {
-    QPCIeTestDev *pcie_testdev = obj;
-    QPCIDevice *pdev = &pcie_testdev->dev;
+    QTDISPTestDev *tdisp_testdev = obj;
+    QPCIDevice *pdev = &tdisp_testdev->dev;
     QPCIBar bar;
 
     g_assert_cmpuint(qpci_config_readw(pdev, PCI_VENDOR_ID), ==,
@@ -54,7 +54,7 @@ static void nop(void *obj, void *data, QGuestAllocator *alloc)
     qpci_iounmap(pdev, bar);
 }
 
-static void pcie_testdev_register_nodes(void)
+static void tdisp_testdev_register_nodes(void)
 {
     QOSGraphEdgeOptions opts = {
         .extra_device_opts = "addr=04.0"
@@ -62,10 +62,10 @@ static void pcie_testdev_register_nodes(void)
 
     add_qpci_address(&opts, &(QPCIAddress) { .devfn = QPCI_DEVFN(4, 0) });
 
-    qos_node_create_driver("edu", pcie_testdev_create);
-    qos_node_consumes("edu", "pci-bus", &opts);
+    qos_node_create_driver("tdisp-testdev", tdisp_testdev_create);
+    qos_node_consumes("tdisp-testdev", "pci-bus", &opts);
 
-    qos_add_test("nop", "edu", nop, NULL);
+    qos_add_test("nop", "tdisp-testdev", nop, NULL);
 }
 
-libqos_init(pcie_testdev_register_nodes);
+libqos_init(tdisp_testdev_register_nodes);
