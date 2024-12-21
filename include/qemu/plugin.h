0838e9ac1362442868fa6bf7ec1f9df0ce46100a
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2024-12-21 13:13:23 -0800

    missing static inline for plugin.h
    Signed-off-by: Pierrick Bouvier <pierrick.bouvier@linaro.org>

--- include/qemu/plugin.h
+++ include/qemu/plugin.h
@@ -246,13 +246,16 @@ static inline void qemu_plugin_vcpu_idle_cb(CPUState *cpu)
 static inline void qemu_plugin_vcpu_resume_cb(CPUState *cpu)
 { }
 
-void qemu_plugin_vcpu_interrupt_cb(CPUState *cpu, uint64_t from, uint64_t to)
+static inline void qemu_plugin_vcpu_interrupt_cb(CPUState *cpu, uint64_t from,
+                                                 uint64_t to)
 { }
 
-void qemu_plugin_vcpu_exception_cb(CPUState *cpu, uint64_t from, uint64_t to)
+static inline void qemu_plugin_vcpu_exception_cb(CPUState *cpu, uint64_t from,
+                                                 uint64_t to)
 { }
 
-void qemu_plugin_vcpu_hostcall_cb(CPUState *cpu, uint64_t from, uint64_t to)
+static inline void qemu_plugin_vcpu_hostcall_cb(CPUState *cpu, uint64_t from,
+                                                uint64_t to)
 { }
 
 static inline void
