8e0b7939d26677a4e51f94d29674823d77c27773
Michael Prantl <michael.prantl@hotmail.de>
2025-01-21 21:44:18 +0000

    Add and implement spdm responder object
    Signed-off-by: Pierrick Bouvier <pierrick.bouvier@linaro.org>

---
+++ hw/spdm/spdm-responder.c
@@ -0,0 +1,37 @@
+#include "qemu/osdep.h"
+#include "qemu/error-report.h"
+#include "qapi/error.h"
+#include "hw/spdm/spdm-responder.h"
+
+OBJECT_DEFINE_ABSTRACT_TYPE(
+    SPDMResponder, spdm_responder, SPDM_RESPONDER, OBJECT)
+
+bool device_spdm_responder_init(DeviceState *dev, SPDMResponder *responder,
+    SPDMResponderSendMessageFunc send_message,
+    SPDMResponderReceiveMessageFunc receive_message,
+    SPDMResponderGetResponseFunc get_response, Error **errp)
+{
+    SPDMResponderClass *class = SPDM_RESPONDER_GET_CLASS(responder);
+    assert(class->device_init);
+    return class->device_init(
+        responder, dev, send_message, receive_message, get_response, errp);
+}
+
+bool spdm_responder_dispatch_message(SPDMResponder *responder, Error **errp)
+{
+    SPDMResponderClass *class = SPDM_RESPONDER_GET_CLASS(responder);
+    assert(class->dispatch_message);
+    return class->dispatch_message(responder, errp);
+}
+
+static void spdm_responder_init(Object *obj)
+{
+}
+
+static void spdm_responder_finalize(Object *obj)
+{
+}
+
+static void spdm_responder_class_init(ObjectClass *oc, const void *data)
+{
+}
