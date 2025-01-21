798d4c60a0fd29c8b78df3d94803d5245133a86d
Michael Prantl <michael.prantl@hotmail.de>
2025-01-21 21:44:18 +0000

    Add and implement spdm responder object
    Signed-off-by: Pierrick Bouvier <pierrick.bouvier@linaro.org>

---
+++ include/hw/spdm/spdm-responder-libspdm.h
@@ -0,0 +1,35 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#ifndef HW_SPDM_SPDM_RESPONDER_LIBSPDM_H
+#define HW_SPDM_SPDM_RESPONDER_LIBSPDM_H
+
+#include "qemu/osdep.h"
+#include "qom/object_interfaces.h"
+#include "hw/spdm/spdm-responder.h"
+
+#define TYPE_SPDM_RESPONDER_LIBSDPM "spdm-responder-libspdm"
+OBJECT_DECLARE_SIMPLE_TYPE(SPDMResponderLibspdm, SPDM_RESPONDER_LIBSDPM)
+
+struct SPDMResponderLibspdm {
+    /*< private >*/
+    SPDMResponder parent_obj;
+    /*< public >*/
+
+    DeviceState *dev;
+
+    void *spdm_context;
+    void *scratch_buffer;
+
+    SPDMResponderSendMessageFunc *send_message;
+    SPDMResponderReceiveMessageFunc *receive_message;
+    SPDMResponderGetResponseFunc *get_response;
+
+    uint32_t max_spdm_msg_size;
+    uint32_t data_transfer_size;
+    uint32_t buffer_size;
+    void *sender_buffer, *receiver_buffer;
+
+    uint32_t capabilities;
+};
+
+#endif /* HW_SPDM_SPDM_RESPONDER_LIBSPDM_H */
