5bd7b781e3cae5eacb7233b2e5427004e73d5a54
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2025-05-16 12:46:27 -0700

    hw/arm/virt: allow to select specific smmu stage (-M virt,iommu=smmuv3-stage{1,2})
    Signed-off-by: Pierrick Bouvier <pierrick.bouvier@linaro.org>

--- include/hw/arm/virt.h
+++ include/hw/arm/virt.h
@@ -35,6 +35,7 @@
 #include "hw/boards.h"
 #include "hw/arm/boot.h"
 #include "hw/arm/bsa.h"
+#include "hw/arm/smmu-common.h"
 #include "hw/block/flash.h"
 #include "hw/cxl/cxl.h"
 #include "system/kvm.h"
@@ -154,6 +155,7 @@ struct VirtMachineState {
     bool mte;
     bool dtb_randomness;
     bool second_ns_uart_present;
+    SMMUStage smmu_stage;
     OnOffAuto acpi;
     VirtGICType gic_version;
     VirtIOMMUType iommu;
