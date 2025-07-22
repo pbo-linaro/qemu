9281f7ae1b45876e81282c9c6a0297c3b0d65617
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2025-07-22 16:07:33 -0700

    include/exec/target_page.h: move page-target.c to header
    Signed-off-by: Pierrick Bouvier <pierrick.bouvier@linaro.org>

--- page-target.c
+++ page-target.c
@@ -9,13 +9,3 @@
 #include "qemu/osdep.h"
 #include "exec/target_page.h"
 
-/* Convert target pages to MiB (2**20). */
-size_t qemu_target_pages_to_MiB(size_t pages)
-{
-    int page_bits = TARGET_PAGE_BITS;
-
-    /* So far, the largest (non-huge) page size is 64k, i.e. 16 bits. */
-    g_assert(page_bits < 20);
-
-    return pages >> (20 - page_bits);
-}
