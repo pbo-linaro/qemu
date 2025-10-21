8c20d257f49463f6c729000bbae80d4a27924327
Mathieu Poirier <mathieu.poirier@linaro.org>
2025-10-21 11:50:21 -0600

    hw/arm/smmuv3: Set stream security state based on STE entry
    Signed-off-by: Mathieu Poirier <mathieu.poirier@linaro.org>
    Signed-off-by: Pierrick Bouvier <pierrick.bouvier@linaro.org>

--- hw/arm/smmu-common.c
+++ hw/arm/smmu-common.c
@@ -55,6 +55,8 @@ ARMSecuritySpace smmu_get_security_space(SMMUSecSID sec_sid)
     switch (sec_sid) {
     case SMMU_SEC_SID_S:
         return ARMSS_Secure;
+    case SMMU_SEC_SID_R:
+        return ARMSS_Realm;
     case SMMU_SEC_SID_NS:
     default:
         return ARMSS_NonSecure;
